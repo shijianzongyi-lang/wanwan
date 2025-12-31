556b5d9b006c07c2daa9c8294dea71ed3584d6ac
Junaco <shijianzongyi@gmail.com>
2025-12-31 23:26:03 +0900

    script.js

--- script.js
+++ script.js
@@ -44,30 +44,30 @@ btn.addEventListener('click', () => {
     cal[0] = 0.2;
   }
   if (result[1] == 'e' || result[1] == 'h') {
-    cal[1] = -30;
+    cal[1] = -32;
   } else if (result[1] == 'f' || result[1] == 'g') {
-    cal[1] = 30;
+    cal[1] = 32;
   }
   if (result[2] == 'j') {
-    cal[2] = -20;
+    cal[2] = -21;
   } else if (result[2] == 'k') {
-    cal[2] = -30;
+    cal[2] = -31;
   } else if (result[2] == 'l') {
-    cal[2] = -40;
+    cal[2] = -41;
   } else if (result[2] == 'm') {
-    cal[2] = -50;
+    cal[2] = -51;
   }
   if (result[3] == 'o') {
-    cal[3] = -20;
+    cal[3] = -19;
   } else if (result[3] == 'p') {
-    cal[3] = -30;
+    cal[3] = -29;
   } else if (result[3] == 'q') {
-    cal[3] = -40;
+    cal[3] = -39;
   } else if (result[3] == 'r') {
-    cal[3] = -50;
+    cal[3] = -49;
   }
   //点数計算
-  const raw_score = (100 + cal[1] + cal[2] + cal[3]) * cal[0];
+  const raw_score = Math.round((100 + cal[1] + cal[2] + cal[3]) * cal[0]);
   if (raw_score > 100) {
     score = 100;
   } else if (raw_score < 0) {
@@ -75,8 +75,30 @@ btn.addEventListener('click', () => {
   } else {
     score = raw_score;
   }
-  p.textContent = `${score}％`;
+  
+  //カウントアップ
+  const refresh = Math.round(2000 / score);
+  var scores = document.getElementById('output');
+  scores.setAttribute('data-num', score);
+  const counters = document.querySelectorAll(".counter");
+  counters.forEach((counter) => {
+    counter.innerText = "0";
+    const updateCount = () => {
+      const target = +counter.getAttribute('data-num');
+      const current = +counter.innerText;
+      const increment = target / 100;
+      if (current < target) {
+        counter.innerText = `${Math.ceil(current + increment)}`;
+        setTimeout(updateCount, refresh); //更新速度(ミリ秒)
+      } else {
+        counter.innerText = target;
+      }
+    };
+    updateCount();
+  });
+  //p.textContent = `${score}％`;
 
+  //Xへの共有
   const share_title = `わたしのワンワン度は...${score}％でした。`;
   const share_twitter = document.getElementById("js-share-twitter");
   share_twitter.setAttribute(
